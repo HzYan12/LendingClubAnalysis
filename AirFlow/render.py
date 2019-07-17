16b16600efd894b90c760ffbe8ea280d3c21b309
HzYan12 <51211808+HzYan12@users.noreply.github.com>
2019-07-17 09:12:26 -0400

    Update render.py

--- AirFlow/render.py
+++ AirFlow/render.py
@@ -1,26 +1,25 @@
 import os
 import sys
 import yaml
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader # Jinjia templete
 from yapf.yapflib.yapf_api import FormatFile
 
+# Input parameters
 templates_path = sys.argv[1]
 manifest_path = sys.argv[2]
 output_path = sys.argv[3]
 
+# File written
 dag_name_template = '_dag_name.j2'
-
 
 def load_templates():
     print('Loading DAG templates from %s' % templates_path)
     return Environment(loader=FileSystemLoader(templates_path))
 
-
 def load_manifest(file_path):
     print('Loading DAG manifest from %s' % file_path)
     with open(file_path) as f:
         return yaml.load(f)
-
 
 def render_dags(manifest):
     templates = load_templates()
@@ -36,7 +35,7 @@ def render_dags(manifest):
             fh.close()
             FormatFile(filename, in_place=True, style_config="chromium")
 
-
+# Load files
 for root, dirs, files in os.walk(os.path.abspath(manifest_path)):
     for file in files:
         print('Getting file %s' % file)
